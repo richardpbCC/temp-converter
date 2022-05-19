e877a5f9cf25ae95cafeca5b821593147df7b7c0
richardpb <rpbeavis@gmail.com>
2022-05-19 17:19:11 +0900

    delete: unused backup component

--- src/Components/Converter.js
+++ src/Components/Converter.js
@@ -16,7 +16,7 @@ function Converter(props) {
   const [egTemp, setEgTemp] = useState("e.g 25");
 
   const inputForm = useRef(null);
-  const radioButton = useRef({ checked: true } ); 
+  const radioButton = useRef({ checked: true });
 
   function toggleTempType(event) {
     if (event.target.id === "fahrenheit") {
